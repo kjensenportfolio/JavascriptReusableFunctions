8ba4778bbb5b08ce071bd1fd21b7a122ebaa6e74
Kara <kara@karajensenportfolio.com>
2015-06-01 08:59:39 -0600

    Update functions.js

--- basic/functions.js
+++ basic/functions.js
@@ -32,3 +32,40 @@ function getViewportWidth() {
          return 0;
  }
 }
+
+function getViewportHeight() {
+       if (window.innerHeight)
+       {
+               return window.innerHeight;
+       }
+       else if (document.body && document.body.offsetHeight)
+       {
+               return document.body.offsetHeight;
+       }
+       else
+       {
+               return 0;
+       }
+}
+
+var tellMeTheSizes=function() {
+       document.getElementById("viewportwidth").innerHTML = getViewportWidth() + "px";
+       document.getElementById("viewportheight").innerHTML = getViewportHeight() + "px";
+       document.getElementById("resolutionheight").innerHTML = screen.height + "px";
+       document.getElementById("resolutionwidth").innerHTML = screen.width + "px";
+}
+
+window.onload=function() {
+       tellMeTheSizes();
+}
+
+window.onresize=function() {
+       tellMeTheSizes();
+}
+
+window.onmousemove=function(event)
+{
+       ev = event || window.event;
+       document.getElementById("mousetop").innerHTML = ev.pageY + "px";
+       document.getElementById("mouseleft").innerHTML = ev.pageX + "px";
+}
