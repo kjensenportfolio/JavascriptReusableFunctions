5f63fc6aedd3882c59443d10868638e26e2bbae0
Kara <kara@karajensenportfolio.com>
2015-06-01 08:58:47 -0600

    Update functions.js
    sources:
    1. https://css-tricks.com/snippets/javascript/viewport-size-screen-resolution-mouse-postition/

--- basic/functions.js
+++ basic/functions.js
@@ -1,7 +1,10 @@
 
 
-//Basic Location
+<script type="text/javascript">
 
+/*
+* *  Basic Location * * 
+*/
 function getBasicLocation() {
 return this.location.origin;
 }
@@ -12,4 +15,20 @@ return this.location.pathname;
 
 function getDocumentImages() {
   this.window.document.im
+}
+
+
+/*
+* *  Window Sizing * * 
+*/
+
+//source: 
+function getViewportWidth() {
+ if (window.innerWidth) {
+         return window.innerWidth;
+ } else if (document.body && document.body.offsetWidth) {
+         return document.body.offsetWidth;
+ } else {
+         return 0;
+ }
 }
